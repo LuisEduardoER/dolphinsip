a788f6a8feef5cb4d406638e643eee464f4000d2
p.drobushevich <p.drobushevich@localhost>
2009-06-16 19:49:57 +0300

    Add engine application to sturt and config felix. Update build script to create engine application.

---
+++ modules/EngineApp/felix/engine.bat
@@ -0,0 +1,14 @@
+@echo off
+
+SETLOCAL
+
+SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_07
+SET CLASSPATH="engine.jar"
+
+echo Start Dolphins Integration Platform Engine....
+"%JAVA_HOME%\bin\java" -Xms64m -Xmx256m -cp %CLASSPATH% by.bsu.fami.etl.engine.app.felix.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+echo Engine closed
+
+pause
+
+ENDLOCAL
